a93cf04e6098589fea491355e04a160f473bf852
sandilem7789 <sandilem7789@gmail.com>
2021-10-08 14:48:52 +0200

    redux acted up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//import react redux package
+import { Provider } from 'react-redux' //provider will provide the entire app with a state
+import store from './redux/store'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <Provider store={store}>
+      <App />
+  </Provider>
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
